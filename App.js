387d6dbb4e7ef16cc584a004e991822ede2dd72b
brunomichalski <bruno_michalski@live.com>
2024-11-28 15:41:13 -0300

    feat: avançando na tela de categorias individuais e estilos

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet } from 'react-native';
-import CategoriesScreen from './screens/CategoriesScreen';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet } from "react-native";
+import CategoriesScreen from "./screens/CategoriesScreen";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import MealOverviewScreen from "./screens/MealOverviewScreen";
 
@@ -10,20 +10,29 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <>
-      <StatusBar style='light' />
+      <StatusBar style="light" />
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="MealsCategories" component={CategoriesScreen}/>
-          <Stack.Screen name="MealsOverview" component={MealOverviewScreen}/>
-        </Stack.Navigator>  
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: "#351401" },
+            headerTintColor: "white",
+            contentStyle: { backgroundColor: "#3f2f25" },
+          }}
+        >
+          <Stack.Screen
+            name="MealsCategories"
+            component={CategoriesScreen}
+            options={{
+              title: "All Categories",
+            }}
+          />
+          <Stack.Screen name="MealsOverview" component={MealOverviewScreen} />
+        </Stack.Navigator>
       </NavigationContainer>
     </>
-
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-
-  },
+  container: {},
 });
--- components/MealItem.js
+++ components/MealItem.js
@@ -1,15 +1,64 @@
-import { Pressable, View, Text, StyleSheet, Platform } from 'react-native';
+import {
+  Pressable,
+  View,
+  Text,
+  StyleSheet,
+  Platform,
+  Image,
+} from "react-native";
 
-function MealItem({ title }) {
-    return (
-        <View >
-            <Text>{title}</Text>
+function MealItem({ title, imageUrl, duration, complexity, affordability }) {
+  return (
+    <View style={styles.mealItem}>
+      <Pressable
+        style={({ pressed }) => (pressed ? styles.buttonPressed : null)}
+        android_ripple={{ color: "#ccc" }}
+      >
+        <View>
+          <Image source={{ uri: imageUrl }} style={styles.image} />
+          <Text style={styles.title}>{title}</Text>
         </View>
-    );
+        <View style={styles.details}>
+          <Text style={styles.detailItem}>{duration}m</Text>
+          <Text style={styles.detailItem}>{complexity}</Text>
+          <Text style={styles.detailItem}>{affordability}</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
 }
 
 export default MealItem;
 
 const styles = StyleSheet.create({
-
+  mealItem: {
+    margin: 16,
+    borderRadius: 8,
+    overflow: "hidden",
+    backgroundColor: "white",
+    elevation: 4,
+  },
+  image: {
+    width: "100%",
+    height: 200,
+  },
+  title: {
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 18,
+    margin: 8,
+  },
+  details: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 8,
+    justifyContent: "center",
+  },
+  detailItem: {
+    marginHorizontal: 4,
+    fontSize: 12,
+  },
+  buttonPressed: {
+    opacity: 0.5,
+  },
 });
